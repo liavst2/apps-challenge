1516b2720d3f82db783ca0cfb19bfa3991634546
liavst2 <liavst2@gmail.com>
2020-01-19 21:09:59 +0200

    updated build.sh

--- build.sh
+++ build.sh
@@ -8,24 +8,27 @@ cd server
 npm i --silent
 npm run build
 npm test
+
 if [ $? -ne 0 ]; then
   echo "Something is wrong with the server functionality... exiting"
   exit 1
 fi
+
 cd ..
 
 echo "Done building server side, moving to client"
-
 echo "Building client side..."
 
 cd client
 npm i --silent
 npm run build
 npm test
+
 if [ $? -ne 0 ]; then
   echo "Something is wrong with the client functionality... exiting"
   exit 1
 fi
+
 cd ..
 
 echo "Done building the components! running the server..."
